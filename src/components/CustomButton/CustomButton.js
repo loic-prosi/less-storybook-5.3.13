fa1584e6040032efd9c87ccf27a472f5443d4f17
loic-prosi <loicprosi@gmail.com>
2020-02-14 16:54:55 +0100

    Update

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./CustomButton.css";
+import "./CustomButton.less";
 
 const CustomButton = props => {
   const { text } = props;
 
-  return <button className="custom-button">{text}</button>;
+  return <button className="css less">{text}</button>;
 };
 
 export default CustomButton;
