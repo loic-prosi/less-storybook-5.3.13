5e3bcd0485247a87a7b99309a43b217c0ce4e9b1
loic-prosi <loicprosi@gmail.com>
2020-02-15 00:29:36 +0100

    Update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.js"],
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
-    "@storybook/addon-links"
-  ],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.less$/,
